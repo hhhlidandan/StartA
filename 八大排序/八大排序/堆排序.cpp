2c961df4b6f329aeb8ebfc4ff303b66b8d7cb434
hhhlidandan <lsipritd1998@163.com>
2020-09-12 22:56:35 +0800

    排序更新

--- 八大排序/八大排序/堆排序.cpp
+++ 八大排序/八大排序/堆排序.cpp
@@ -37,74 +37,7 @@
 //	}
 //}
 
-//#include<stdio.h>
-//#include<stdlib.h>
-//void Swap(int *a,int *b)
-//{
-//	*a = *a^*b;
-//	*b ^= *a;
-//	*a ^= *b;
-//}
-//void Adjust(int *arr, int size, int parent)
-//{
-//	int child = parent * 2 + 1;
-//	while (child < size)
-//	{
-//		if (arr[child + 1]>arr[child] && child + 1 < size)
-//			child = child + 1;
-//		if (arr[child] > arr[parent])
-//		{
-//			Swap(&arr[child], &arr[parent]);
-//			parent = child;
-//			child = parent * 2 + 1;
-//		}
-//		return;
-//	}
-//}
-//void HeapSort(int *arr,int size)
-//{
-//	for (int i = (size - 2) >> 1; i >= 0; i--)
-//	{
-//		Adjust(arr,size,i);
-//	}
-//	int end = size - 1;
-//	while (end)
-//	{
-//		Swap(&arr[0], &arr[end]);
-//		Adjust(arr, end, 0);
-//		end--;
-//	}
-//}
 
-//void Adjust(int *arr,int parent,int size)
-//{
-//	int child = parent * 2 + 1;
-//	while (child < size)
-//	{
-//		if (arr[child] > arr[child + 1] && child + 1 < size)
-//			child = child + 1;
-//		if (arr[parent] > arr[child] && child < size)
-//		{
-//			Swap(&arr[parent], &arr[child]);
-//			parent = child;
-//			child = parent * 2 + 1;
-//		}
-//		else
-//			return;
-//	}
-//}
-//void HeapSort(int *arr, int size)//½µĞò ½¨Ğ¡¶Ñ
-//{
-//	for (int root = (size - 2) >> 1; root >= 0; root--)
-//		Adjust(arr, root,size);
-//	int end = size - 1;
-//	while (end)
-//	{
-//		Swap(&arr[end], &arr[0]);
-//		Adjust(arr, 0, end);
-//		end--;
-//	}
-//}
 //int main()
 //{
 //	int arr[] = { 2, 0, 5, 9, 3, 7, 6, 4 };
